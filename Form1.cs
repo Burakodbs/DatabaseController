9059e52bf11b8d2a7d454444befe5ba5863a1745
ronin <ronin@DESKTOP-4F6ETC0>
2023-12-05 14:24:15 +0300

    Proje dosyası ekle.

---
+++ Form1.cs
@@ -0,0 +1,23 @@
+using System.Data;
+using System;
+
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace DatabaseController
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
