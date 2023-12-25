be38565e13d644e37b96b784ac165f3cf548e170
ronin <burak.odabas2@ogr.sakarya.edu.tr>
2023-12-25 22:40:03 +0300

    First

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            npgsqlConnectionBindingSource = new BindingSource(components);
+            dataGrid = new DataGridView();
+            typeText = new TextBox();
+            namesurnameText = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)npgsqlConnectionBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGrid).BeginInit();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 57);
+            button1.Name = "button1";
+            button1.Size = new Size(99, 23);
+            button1.TabIndex = 0;
+            button1.Text = "EKLE";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 86);
+            button2.Name = "button2";
+            button2.Size = new Size(99, 23);
+            button2.TabIndex = 2;
+            button2.Text = "SİL";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(12, 115);
+            button3.Name = "button3";
+            button3.Size = new Size(99, 23);
+            button3.TabIndex = 3;
+            button3.Text = "GÜNCELLE";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(12, 144);
+            button4.Name = "button4";
+            button4.Size = new Size(99, 23);
+            button4.TabIndex = 4;
+            button4.Text = "ARAMA";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(406, 386);
+            button5.Name = "button5";
+            button5.Size = new Size(99, 23);
+            button5.TabIndex = 6;
+            button5.Text = "LİSTELE";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // npgsqlConnectionBindingSource
+            // 
+            npgsqlConnectionBindingSource.DataSource = typeof(Npgsql.NpgsqlConnection);
+            // 
+            // dataGrid
+            // 
+            dataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGrid.Location = new Point(175, 12);
+            dataGrid.Name = "dataGrid";
+            dataGrid.Size = new Size(585, 368);
+            dataGrid.TabIndex = 7;
+            // 
+            // typeText
+            // 
+            typeText.Location = new Point(69, 214);
+            typeText.Name = "typeText";
+            typeText.Size = new Size(100, 23);
+            typeText.TabIndex = 8;
+            // 
+            // namesurnameText
+            // 
+            namesurnameText.Location = new Point(110, 243);
+            namesurnameText.Name = "namesurnameText";
+            namesurnameText.Size = new Size(59, 23);
+            namesurnameText.TabIndex = 9;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(9, 217);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 10;
+            label1.Text = "ID/TYPEID";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(8, 246);
+            label2.Name = "label2";
+            label2.Size = new Size(96, 15);
+            label2.TabIndex = 11;
+            label2.Text = "NAMESURNAME";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.FromArgb(0, 192, 192);
+            ClientSize = new Size(772, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(namesurnameText);
+            Controls.Add(typeText);
+            Controls.Add(dataGrid);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)npgsqlConnectionBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGrid).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private DataGridView dataGridView1;
+        private Button button5;
+        private DataGridViewTextBoxColumn typeMapperDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn connectionStringDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn providePasswordCallbackDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn hostDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn portDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn connectionTimeoutDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn commandTimeoutDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn databaseDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn dataSourceDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn userNameDataGridViewTextBoxColumn;
+        private DataGridViewCheckBoxColumn canCreateBatchDataGridViewCheckBoxColumn;
+        private DataGridViewTextBoxColumn provideClientCertificatesCallbackDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn userCertificateValidationCallbackDataGridViewTextBoxColumn;
+        private BindingSource npgsqlConnectionBindingSource;
+        private DataGridView dataGrid;
+        private TextBox typeText;
+        private TextBox namesurnameText;
+        private Label label1;
+        private Label label2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,9 @@ using System;
 
 using System.Data.SqlClient;
 using System.Windows.Forms;
+using Npgsql;
+
+
 
 namespace DatabaseController
 {
@@ -18,6 +21,63 @@ namespace DatabaseController
 
         }
 
+        NpgsqlConnection url = new NpgsqlConnection("Server=127.0.0.1; Port=5432;Database=ETRADE;user Id=postgres ; Password=07102002");
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM db.\"EMPLOYEES\"";
+            NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(query, url);
+            DataSet dataSet = new DataSet();
+            dataAdapter.Fill(dataSet);
+            dataGrid.DataSource = dataSet.Tables[0];
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            url.Open();
+            NpgsqlCommand command = new NpgsqlCommand("insert into db.\"EMPLOYEES\" (\"TYPEID\",\"NAMESURNAME\") VALUES(@p1,@p2)", url);
+            command.Parameters.AddWithValue("@p1", Int32.Parse(typeText.Text));
+            command.Parameters.AddWithValue("@p2", namesurnameText.Text);
+            command.ExecuteNonQuery();
+            url.Close();
+            MessageBox.Show("Ýþlem gerçekleþti...");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            url.Open();
+            NpgsqlCommand command = new NpgsqlCommand("delete from db.\"EMPLOYEES\" where \"ID\" = @p1 ", url);
+            command.Parameters.AddWithValue("@p1", Int32.Parse(typeText.Text));
+            command.ExecuteNonQuery();
+            url.Close();
+            MessageBox.Show("Ýþlem gerçekleþti...");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            url.Open();
+            NpgsqlCommand command = new NpgsqlCommand("update db.\"EMPLOYEES\" set \"NAMESURNAME\" = @p2 where  \"ID\" = @p1 ", url);
+            command.Parameters.AddWithValue("@p1", Int32.Parse(typeText.Text));
+            command.Parameters.AddWithValue("@p2", namesurnameText.Text);
+            command.ExecuteNonQuery();
+            url.Close();
+            MessageBox.Show("Ýþlem gerçekleþti...");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            url.Open();
+            NpgsqlCommand command = new NpgsqlCommand("select * from db.\"EMPLOYEES\" where \"TYPEID\" = @p1", url);
+            command.Parameters.AddWithValue("@p1", Int32.Parse(typeText.Text));
+            int search = Int32.Parse(typeText.Text);
+            string query = "select * from db.\"EMPLOYEES\" where \"TYPEID\" = ";
+            query += search;
+            NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(query, url);
+            DataSet dataSet = new DataSet();
+            dataAdapter.Fill(dataSet);
+            dataGrid.DataSource = dataSet.Tables[0];
+            url.Close();
+
+        }
     }
 }
